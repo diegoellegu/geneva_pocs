51709e7f01b88181e9017d16a77a2218208d99de
Diego Parrilla <diego.parrilla@pyxis.tech>
2024-03-02 12:17:27 -0300

    Ajustes en analisis de métricas de CC y Mantenibilidad

--- radon/generar_informe_cc.py
+++ radon/generar_informe_cc.py
@@ -12,7 +12,11 @@ def generate_html_report(data):
             th, td { padding: 12px; text-align: left; border: 1px solid #ddd; }
             th { background-color: #f2f2f2; }
             tr:hover { background-color: #f5f5f5; }
+            canvas { max-width: 800px; margin: 20px auto; }
         </style>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation"></script>
+
         <title>Informe de Complejidad Ciclomática</title>
     </head>
     <body>
@@ -44,10 +48,94 @@ def generate_html_report(data):
     # Finaliza el informe HTML
     html_report += '''
         </table>
+            <div id="contenedorCanvas" style="overflow: auto; width: 100%; height: 500px;">
+                    <canvas id="complexityChart"></canvas>
+                </div>
+
+        <script>
+            document.getElementById('complexityChart').width = document.getElementById('contenedorCanvas').clientWidth;
+            document.getElementById('complexityChart').height = document.getElementById('contenedorCanvas').clientHeight;
+            // Datos para el gráfico
+            var data = ''' + json.dumps(data) + ''';
+            var filenames = Object.keys(data);
+            var rutaBase = "/home/dparrilla/development/ude_proyecto_grado/geneva/";
+
+            // Utiliza map para modificar cada elemento en el array filenames
+            var labelsModificados = filenames.map(function (rutaCompleta) {
+                // Reemplaza la parte específica de la ruta con una cadena vacía
+                return rutaCompleta.replace(rutaBase, "");
+            });
+            var complexities = Object.values(data).map(classInfo => classInfo[0].complexity);
+
+            // Configuración del gráfico
+            var ctx = document.getElementById('complexityChart').getContext('2d');
+              var chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labelsModificados,
+                    datasets: [{
+                        label: 'Complejidad Ciclomática',
+                        data: complexities,
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1
+                    },
+                    ]
+                },
+                 options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            max: 21
+                        }
+                    },
+                     plugins: { annotation: {
+                        annotations: [{
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y',
+                            value: 5,
+                            borderColor: 'green',
+                            borderWidth: 2,
+                            label: {
+                                content: 'Rango A',
+                                enabled: true,
+                                position: 'right'
+                            }
+                        },{
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y',
+                            value: 10,
+                            borderColor: 'yellow',
+                            borderWidth: 2,
+                            label: {
+                                content: 'Rango B',
+                                enabled: true,
+                                position: 'right'
+                            }
+                        },{
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y',
+                            value: 20,
+                            borderColor: 'tomato',
+                            borderWidth: 2,
+                            label: {
+                                content: 'Rango C',
+                                enabled: true,
+                                position: 'right'
+                            }
+                        }]
+                    }}
+
+                }
+
+            });
+        </script>
     </body>
     </html>
     '''
-
     return html_report
 
 # Lee el contenido del archivo JSON
--- radon/generar_informe_mantenibilidad.py
+++ radon/generar_informe_mantenibilidad.py
@@ -14,6 +14,8 @@ def generate_html_report(data):
             th { background-color: #f2f2f2; }
             tr:hover { background-color: #f5f5f5; }
         </style>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation"></script>
         <title>Informe de Mantenibilidad</title>
     </head>
     <body>
@@ -38,6 +40,79 @@ def generate_html_report(data):
     # Finaliza el informe HTML
     html_report += '''
         </table>
+            <div id="contenedorCanvas" style="overflow: auto; width: 100%; height: 500px;">
+                    <canvas id="complexityChart"></canvas>
+                </div>
+
+        <script>
+            document.getElementById('complexityChart').width = document.getElementById('contenedorCanvas').clientWidth;
+            document.getElementById('complexityChart').height = document.getElementById('contenedorCanvas').clientHeight;
+            // Datos para el gráfico
+            var data = ''' + json.dumps(data) + ''';
+            var filenames = Object.keys(data);
+               var rutaBase = "/home/dparrilla/development/ude_proyecto_grado/geneva/";
+
+            // Utiliza map para modificar cada elemento en el array filenames
+            var labelsModificados = filenames.map(function (rutaCompleta) {
+                // Reemplaza la parte específica de la ruta con una cadena vacía
+                return rutaCompleta.replace(rutaBase, "");
+            });
+            var mi = Object.values(data).map(classInfo => classInfo.mi);
+
+            // Configuración del gráfico
+            var ctx = document.getElementById('complexityChart').getContext('2d');
+              var chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labelsModificados,
+                    datasets: [{
+                        label: 'Complejidad Ciclomática',
+                        data: mi,
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1
+                    },
+                    ]
+                },
+                 options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            max: 100
+                        }
+                    },
+                     plugins: { annotation: {
+                        annotations: [{
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y',
+                            value: 20,
+                            borderColor: 'yellow',
+                            borderWidth: 2,
+                            label: {
+                                content: 'Rango B',
+                                enabled: true,
+                                position: 'right'
+                            }
+                        },{
+                            type: 'line',
+                            mode: 'horizontal',
+                            scaleID: 'y',
+                            value: 10,
+                            borderColor: 'tomato',
+                            borderWidth: 2,
+                            label: {
+                                content: 'Rango C',
+                                enabled: true,
+                                position: 'right'
+                            }
+                        }]
+                    }}
+
+                }
+
+            });
+        </script>
     </body>
     </html>
     '''
